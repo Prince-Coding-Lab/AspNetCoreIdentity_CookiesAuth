d808b22ce6c436e6c9740e03b3b03a81fe32a5a0
Prince-Coding-Lab <54809854+Prince-Coding-Lab@users.noreply.github.com>
2020-05-10 23:47:32 +0530

    Prince: Init new project for cookies auth.

---
+++ PrinceLabz.AspNetCoreCookies.Auth/PrinceLabz.AspNetCoreCookies.Auth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PrinceLabz.AspNetCoreCookies.Auth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
